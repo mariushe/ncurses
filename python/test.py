86c9a6b8a1c8671567137fa065e3730b4e8f7905
Marius Herring <mariusherring@gmail.com>
2013-12-20 08:22:16 +0100

    Finished python test

--- python/test.py
+++ python/test.py
@@ -1,9 +1,17 @@
+#! /usr/bin/env python
+
 import curses
 
 myscreen = curses.initscr()
 
 myscreen.border(0)
-myscreen.addstr(12, 25, "Python curses in action!")
+curses.start_color()
+curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+myscreen.addstr(12, 25, "Python curses in action!", curses.color_pair(1))
+curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+myscreen.addstr(13, 25, "Python curses in action!", curses.color_pair(2))
+curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+myscreen.addstr(14, 25, "Python curses in action!", curses.color_pair(3))
 myscreen.refresh()
 myscreen.getch()
 
